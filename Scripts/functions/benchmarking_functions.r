d56e606a624275b10bc8b8275bf4cecaae1b2625
JinhongZ <tetsuya.iris@gmail.com>
2026-02-05 12:09:43 +1100

    Modify functions to generate sample- and cell-level data information

--- Scripts/functions/benchmarking_functions.r
+++ Scripts/functions/benchmarking_functions.r
@@ -1,30 +1,5 @@
 # Required packages are Seurat, Matrix, data.table, dplyr, ggplot2
 
-# helper function to validate if the length of given seurat list matches the length of segmentation options
-validate_segmentation <- function(segmentation, seg_levels, n_objs) {
-  
-  if (!all(segmentation %in% seg_levels)) {
-    bad <- unique(segmentation[!segmentation %in% seg_levels])
-    stop(
-      "Invalid segmentation value(s): ",
-      paste(bad, collapse = ", "),
-      "\nAllowed values: ",
-      paste(seg_levels, collapse = ", "),
-      call. = FALSE
-    )
-  }
-  
-  if (!(length(segmentation) == n_objs)) {
-    stop(
-      "segmentation must have length 1 or match merged_seurat_list length (",
-      n_objs, ").",
-      call. = FALSE
-    )
-  }
-  
-  invisible(TRUE)
-}
-
 # find the transcripts counts for genes overlapping MERSCOPE and Xenium panels
 add_common_gene_counts <- function(obj, common_genes) {
   # use dgCMatrix for faster processing
@@ -46,32 +21,20 @@ add_common_gene_counts <- function(obj, common_genes) {
   )
 } 
 
-generate_sample_df <- function(merged_seurat_list, segmentation, seg_levels, count_col) {
-  # check if segmentation options match
-  n <- length(merged_seurat_list)
-  validate_segmentation(segmentation, seg_levels, n)
+generate_sample_df <- function(obj, segmentation, count_col) {
+  # extract cell metadata
+  dt <- data.table::as.data.table(obj@meta.data)
+  has_common <- "common_gene_counts" %in% names(dt)
   
-  segmentation <- factor(segmentation, levels = seg_levels)
-  
-  # use data.table for faster grouping and aggregation 
-  data.table::rbindlist(
-    # iterate over each seurat object and segmentation in parallel 
-    # syntax Map(function(a, b) {...}, A, B)
-    Map(function(obj, seg) {
-      dt <- data.table::as.data.table(obj@meta.data)
-      has_common <- "common_gene_counts" %in% names(dt)
-      # group cells by sample_id and compute summaries per sample
-      # syntax dt[i, j, by]
-      dt[, .(
-        cell_count = .N, # number of rows in current group, faster than n()
-        transcript_count = sum(get(count_col)),
-        common_gene_counts = if (has_common) sum(common_gene_counts) else sum(get(count_col)),
-        batch = batch[1],
-        segmentation = seg
-      ), by = sample_id]
-    }, merged_seurat_list, segmentation),
-    use.names = TRUE
-  )
+  # group cells by sample_id and compute summaries per sample
+  # syntax dt[i, j, by]
+  dt[, .(
+    cell_count = .N, # number of rows in current group, faster than n()
+    transcript_count = sum(get(count_col)),
+    common_gene_counts = if (has_common) sum(common_gene_counts) else sum(get(count_col)),
+    batch = batch[1],
+    segmentation = segmentation
+  ), by = sample_id]
 }
 
 # This function is only suitable for Xenium data
@@ -113,34 +76,22 @@ generate_transcripts_assigned <- function(dir_list, segmentation) {
   )
 }
 
-generate_cell_df <- function(merged_seurat_list, segmentation, seg_levels, count_col, feature_col) {
-  # check if segmentation options match
-  n <- length(merged_seurat_list)
-  validate_segmentation(segmentation, seg_levels, n)
-  
-  segmentation <- factor(segmentation, levels = seg_levels)
-  
-  # extract cell information sample-by-sample
-  data.table::rbindlist(
-    Map(function(obj, seg) {
-      # extract cell metadata
-      dt <- data.table::as.data.table(obj@meta.data)
-      dt[, .(
-        sample = gsub("_", "", sample_id),
-        nCounts = get(count_col),
-        nFeatures = get(feature_col),
-        common_gene_counts = if ("common_gene_counts" %in% names(dt)) common_gene_counts else get(count_col),
-        common_feature_counts = if ("common_feature_counts" %in% names(dt)) common_feature_counts else get(feature_col),
-        cell_type = ScType_label_res.0.6,
-        cell_area = if("cell_area" %in% colnames(dt)) cell_area else NA_real_,
-        aspect_ratio = if("aspect_ratio" %in% colnames(dt)) aspect_ratio else NA_real_,
-        log10_signal_density = if("log10_signal_density" %in% colnames(dt)) log10_signal_density else NA_real_,
-        log10_signal_density_outlier_sc = if("log10_signal_density_outlier_sc" %in% colnames(dt)) log10_signal_density_outlier_sc else NA_real_,
-        segmentation = seg
-      )]
-    }, merged_seurat_list, segmentation),
-    use.names = TRUE
-  )
+generate_cell_df <- function(obj, segmentation, count_col, feature_col) {
+  # extract cell metadata
+  dt <- data.table::as.data.table(obj@meta.data)
+  dt[, .(
+    sample = gsub("_", "", sample_id),
+    nCounts = get(count_col),
+    nFeatures = get(feature_col),
+    common_gene_counts = if ("common_gene_counts" %in% names(dt)) common_gene_counts else get(count_col),
+    common_feature_counts = if ("common_feature_counts" %in% names(dt)) common_feature_counts else get(feature_col),
+    cell_type = ScType_label_res.0.6,
+    cell_area = if("cell_area" %in% colnames(dt)) cell_area else NA_real_,
+    aspect_ratio = if("aspect_ratio" %in% colnames(dt)) aspect_ratio else NA_real_,
+    log10_signal_density = if("log10_signal_density" %in% colnames(dt)) log10_signal_density else NA_real_,
+    log10_signal_density_outlier_sc = if("log10_signal_density_outlier_sc" %in% colnames(dt)) log10_signal_density_outlier_sc else NA_real_,
+    segmentation = segmentation
+  )]
 }
 
 # --- plots for sample-level quality metrics
